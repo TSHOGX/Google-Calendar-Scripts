08fe21f7af9725d4a59f15ee4876de28b17316ae
TSHOGX <tshogx@outlook.com>
2020-07-21 15:41:44 +0800

    add some funcs

--- README.md
+++ README.md
@@ -3,9 +3,16 @@
 [Quickstart](https://developers.google.com/calendar/quickstart/python)
 
 used API:
-* [Events](https://developers.google.com/calendar/v3/reference/events)
-* [CalendarList: list](https://developers.google.com/calendar/v3/reference/calendarList/list)
+* [Events](https://developers.google.com/calendar/v3/reference/events): Events is 
+* [CalendarList: list](https://developers.google.com/calendar/v3/reference/calendarList/list): CalendarList is list of categories
 
 Next:
 * count for select items
 * calculate each items periods and plot a graph
+
+Plot(for selected week, this 7 days):
+* day - labels
+* category - items
+* all events in this category - number
+
+
--- count.py
+++ count.py
@@ -5,16 +5,51 @@ import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+import matplotlib.pyplot as plt
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
 
 def main():
+    service = initialize()
+    categoryIdList = getCategoriesId(service)
+
+
+    print('----Show summaries for every category----')
+    page_token = None
+    # traverse all category
+    timeList = [] # time list for every category
+    for i in range(len(categoryIdList)):
+        categoryId = categoryIdList[i]
+        # for each category
+        while True:
+            categories = service.events().list(calendarId=categoryId, pageToken=page_token).execute()
+
+            # iterate every event in this category
+            print('#', categories['summary'], ': ')
+            time = 0
+            for event in categories['items']:
+                print('   ', event['summary'], timeCalculator(event))
+                time += timeCalculator(event)
+            timeList.append(time)
+
+            page_token = categories.get('nextPageToken')
+            if not page_token:
+                break
+    print('----', len(categoryIdList),'category down----')
+    
+
+
+
+    # plotThisWeek()
+
+
+
+
+def initialize():
     creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
+    # The file token.pickle stores the user's access and refresh tokens, and is created automatically when the authorization flow completes for the first time.
     if os.path.exists('./ignore/token.pickle'):
         with open('./ignore/token.pickle', 'rb') as token:
             creds = pickle.load(token)
@@ -31,38 +66,67 @@ def main():
             pickle.dump(creds, token)
 
     service = build('calendar', 'v3', credentials=creds)
+    return service
 
-
-    # show calendar items name and id
-    print('----Show calendar items name----')
+def getCategoriesId(service):
+    '''
+    get user's categories
+    store id in categoryIdList
+    '''
     page_token = None
     while True:
-        eventid = []
+        categoryIdList = []
         calendar_list = service.calendarList().list(pageToken=page_token).execute()
         for calendar_list_entry in calendar_list['items']:
-            print(calendar_list_entry['summary'])
-            eventid.append(calendar_list_entry['id'])
+            # print(calendar_list_entry['summary'])
+            categoryIdList.append(calendar_list_entry['id'])
         page_token = calendar_list.get('nextPageToken')
         if not page_token:
-            print('----Count: this calendar has ',len(eventid), ' items----')
-            print()
+            # print('----Count: this calendar has',len(categoryIdList), 'categories----')
+            # print()
             break
+    return categoryIdList
 
-    # traverse all items. Show summaries for every items
-    print('----Show summaries for every items----')
-    page_token = None
-    for i in range(len(eventid)):
-        itemId = eventid[i]
 
-        while True:
-            events = service.events().list(calendarId=itemId, pageToken=page_token).execute()
-            print('# ', events['summary'], ': ')
-            for event in events['items']:
-                print('  ', event['summary'])
-            page_token = events.get('nextPageToken')
-            if not page_token:
-                break
-    print('----', len(eventid),' items down----')
-    
+def timeCalculator(event):
+    '''
+    calculate event lasted time
+    return float type (hour)
+    '''
+    startTime = event['start']['dateTime']
+    endTime = event['end']['dateTime']
+    # 2020-07-21T10:30:00+08:00
+    sHour = startTime[11:13]
+    sMinute = startTime[14:16]
+    # sSecond = startTime[17:19]
+    eHour = endTime[11:13]
+    eMinute = endTime[14:16]
+    # eSecond = endTime[17:19]
+    lastMinute = (int(eHour) * 60 + int(eMinute)) - (int(sHour) * 60 + int(sMinute))
+    lastHour = lastMinute / 60
+    return lastHour
+
+
+def plotThisWeek(labels, category):
+    labels = ['D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7']
+    men_means = [20, 35, 30, 35, 27]
+    women_means = [25, 32, 34, 20, 25]
+    men_std = [2, 3, 4, 1, 2]
+    women_std = [3, 5, 2, 3, 3]
+    width = 0.35       # the width of the bars: can also be len(x) sequence
+
+    fig, ax = plt.subplots()
+
+    ax.bar(labels, men_means, width, yerr=men_std, label='Men')
+    ax.bar(labels, women_means, width, yerr=women_std, bottom=men_means,
+        label='Women')
+
+    ax.set_ylabel('Scores')
+    ax.set_title('Scores by group and gender')
+    ax.legend()
+
+    plt.show()
+
+
 if __name__ == '__main__':
     main()
